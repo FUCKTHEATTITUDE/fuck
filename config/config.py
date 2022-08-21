29d5f3802c8f153176b73b06cfe9f51dc94951a3
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2022-08-22 04:45:59 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -61,7 +61,7 @@ UPSTREAM_REPO = getenv(
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 # GIT TOKEN ( if your edited repo is private)
-GIT_TOKEN = getenv("GIT_TOKEN", None)
+GIT_TOKEN = getenv("GIT_TOKEN","https://github.com/FUCKTHEATTITUDE/nds.git")
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
